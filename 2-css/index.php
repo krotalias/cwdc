b0dedf9f75173473c51749ab52dc1f364b88e235
krotalias <32539739+krotalias@users.noreply.github.com>
2022-12-02 07:04:45 -0300

    Using Bottstrap 4.6.2

--- 2-css/index.php
+++ 2-css/index.php
@@ -5,103 +5,110 @@ include "../mainPage/navbar.php";
 ?>
 
 <div class="lesson">
-<h3> 
+<h3>
     <a href="https://www.w3.org/Style/CSS/Overview.en.html">CSS</a>
     <a href="/nodejs/books/css-handbook.pdf">(book)</a>
+    <a href="css-declaration-small.png">
+        <img
+            height="45"
+            style="border: 0; text-align: center"
+            src="css-declaration-small.png"
+        />
+    </a>
 </h3>
 
 <ol type="1" start="1">
-	<li> 
-        <a href="2.2.html">Inline CSS</a> 
+    <li>
+        <a href="2.2.html">Inline CSS</a>
         <a href="showCode.php?f=2.2">(html)</a>
     </li>
-	<li> <a href="2.3.html">
-        Internal CSS</a> 
+    <li> <a href="2.3.html">
+        Internal CSS</a>
         <a href="showCode.php?f=2.3">(html)</a>
     </li>
-	<li> <a href="2.4.html">Classes and IDs</a> 
+    <li> <a href="2.4.html">Classes and IDs</a>
         <a href="showCode.php?f=2.4">(html)</a>
     </li>
-	<li> 
-        <a href="2.5.html">Divs</a> 
+    <li>
+        <a href="2.5.html">Divs</a>
         <a href="showCode.php?f=2.5">(html)</a>
     </li>
-	<li> 
-        <a href="2.6.html">Colors</a> 
+    <li>
+        <a href="2.6.html">Colors</a>
         <a href="showCode.php?f=2.6">(html)</a>
     </li>
-	<li> 
-        <a href="2.7.html">Floating </a> 
+    <li>
+        <a href="2.7.html">Floating </a>
         <a href="showCode.php?f=2.7">(html)</a>
         <a href="showCodeCss.php?f=roma">(css)</a>
     </li>
-	<li> 
-        <a href="2.8.html">Positioning</a> 
+    <li>
+        <a href="2.8.html">Positioning</a>
         <a href="showCode.php?f=2.8">(html)</a>
         <a href="showCodeCss.php?f=roma">(css)</a>
     </li>
-	<li> 
-        <a href="2.9.html">Margins</a> 
+    <li>
+        <a href="2.9.html">Margins</a>
         <a href="showCode.php?f=2.9">(html)</a>
         <a href="showCodeCss.php?f=roma">(css)</a>
     </li>
-	<li> 
-        <a href="2.10.html">Padding</a> 
+    <li>
+        <a href="2.10.html">Padding</a>
         <a href="showCode.php?f=2.10">(html)</a>
         <a href="showCodeCss.php?f=roma">(css)</a>
     </li>
-	<li> 
-        <a href="2.11.html">Borders</a> 
+    <li>
+        <a href="2.11.html">Borders</a>
         <a href="showCode.php?f=2.11">(html)</a>
     </li>
-	<li> 
-        <a href="2.12.html">Font</a> 
+    <li>
+        <a href="2.12.html">Font</a>
         <a href="showCode.php?f=2.12">(html)</a>
     </li>
-	<li> 
-        <a href="2.13.html">Styling Text</a> 
+    <li>
+        <a href="2.13.html">Styling Text</a>
         <a href="showCode.php?f=2.13">(html)</a>
     </li>
-	<li> 
-        <a href="2.14.html">Aligning Text</a> 
-        <a href="showCode.php?f=2.14">(html)</a> 
-        <a 
+    <li>
+        <a href="2.14.html">Aligning Text</a>
+        <a href="showCode.php?f=2.14">(html)</a>
+        <a
             href="https://developer.apple.com/library/archive/documentation/NetworkingInternetWeb/Conceptual/Web_Inspector_Tutorial/EditingCode/EditingCode.html"
         >
             (edit)
         </a>
         <a href="debug.png">
-            <img 
-                width="80" 
-                height="45" 
+            <img
+                width="80"
+                height="45"
                 style="border: 0; text-align: center"
                 src="debug.png"
             />
         </a>
     </li>
-    <li> 
-        <a href="specificity.html">Combining Selectors and Specificity</a> 
-        <a href="showCode.php?f=specificity">(html)</a> 
+    <li>
+        <a href="specificity.html">Combining Selectors and Specificity</a>
+        <a href="showCode.php?f=specificity">(html)</a>
     </li>
-	<li> 
-        <a href="2.15.html">Webpage with Style</a> 
+    <li>
+        <a href="2.15.html">Webpage with Style</a>
         <a href="showCode.php?f=2.15">(html)</a>
     </li>
-	<li> 
-        <a href="bbc-2016.html">BBC Clone 2016</a> 
+    <li>
+        <a href="bbc-2016.html">BBC Clone 2016</a>
         <a href="showCode.php?f=bbc-2016">(html)</a>
         <a href="showCodeCss.php?f=bbc-2016">(css)</a>
     </li>
-	<li> 
-        <a href="bbc.html">BBC Clone 2020</a> 
+    <li>
+        <a href="bbc.html">BBC Clone 2020</a>
         <a href="showCode.php?f=bbc">(html)</a>
         <a href="showCodeCss.php?f=bbc-2020">(css)</a>
         <a href="showCodeCss.php?f=dropdown">(css)</a>
         <a href="showcodePHP.php?f=layout">(hierarchy)</a>
     </li>
-    <li> 
-        <a href="../mainPage/Fedora_People.html">Fedora</a>
-        <a href="showCode.php?f=../mainPage/Fedora_People">(html)</a>
+    <li>
+        <a href="/roma/txt/Fedora_People.html">Fedora</a>
+        <a href="showCode.php?f=../mainPage/txt/Fedora_People">(html)</a>
         <a href="showCodeCss.php?f=css/fedora">(css)</a>
         <a href="showCodeCss.php?f=css/style">(css)</a>
     </li>
