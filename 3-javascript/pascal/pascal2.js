5b6e7e320cd625d5db91aed75b6a89d24ad5a504
krotalias <32539739+krotalias@users.noreply.github.com>
2023-08-26 11:07:07 -0300

    Removed node modules.

--- 3-javascript/pascal/pascal2.js
+++ 3-javascript/pascal/pascal2.js
@@ -1,1 +1,10 @@
-python/pascal.js
+dragAndSave("#fig");
+dragAndSave("#tri");
+dragAndSave("#code");
+
+// calculate the size of the boxes for the code and triangle.
+var test = document.getElementsByClassName("test");
+var wTri = test[0].clientWidth + 1 + "px";
+var wCode = test[1].clientWidth + 1 + "px";
+document.documentElement.style.setProperty("--wTri", wTri);
+document.documentElement.style.setProperty("--wCode", wCode);
