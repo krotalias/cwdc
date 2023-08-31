d6b8d5bf308a90d7b13b7c31290aa63ae1c755de
krotalias <32539739+krotalias@users.noreply.github.com>
2023-08-31 18:46:53 -0300

    Fixed doc paths.

--- 13-webgl/Assignment_3/doc.sh
+++ 13-webgl/Assignment_3/doc.sh
@@ -1,1 +1,1 @@
-jsdoc -d /Users/roma/html/cwdc/13-webgl/doc-hierarchy-partial Hierarchy.js ../teal_book/cuon-matrix.js ../teal_book/cuon-utils.js ../lib/simple-rotator.js 
+jsdoc -d ../doc-hierarchy-partial Hierarchy.js ../lib/teal_book/cuon-matrix.js ../lib/teal_book/cuon-utils.js ../lib/simple-rotator.js 
