79e50230e52c091cae72dbb731a5a1a3aefe2294
krotalias <32539739+krotalias@users.noreply.github.com>
2023-08-31 11:38:08 -0300

    Fixed path.

--- 13-webgl/Assignment_1/doc.sh
+++ 13-webgl/Assignment_1/doc.sh
@@ -1,3 +1,3 @@
-jsdoc -d doc-twist twist.js ../lib/gl-matrix/dist/esm ../Common/initShaders.js
-jsdoc -d doc-twist-MV twist-MV.js ../Common/MV.js ../Common/initShaders.js
+jsdoc -d doc-twist twist.js ../lib/gl-matrix/dist/esm ../lib/Common/initShaders.js
+jsdoc -d doc-twist-MV twist-MV.js ../lib/Common/MV.js ../lib/Common/initShaders.js
 
