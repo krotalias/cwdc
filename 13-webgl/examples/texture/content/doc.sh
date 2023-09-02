235807ca5ea1ca5df5714519cdb3272a3a989e88
krotalias <32539739+krotalias@users.noreply.github.com>
2023-09-02 11:07:43 -0300

    Missing images.

--- 13-webgl/examples/texture/content/doc.sh
+++ 13-webgl/examples/texture/content/doc.sh
@@ -1,3 +1,3 @@
 jsdoc -d doc-texture LightingWithTexture.js ../../../../13-webgl/lib/gl-matrix/dist/esm ../../../../13-webgl/lib/teal_book/cuon-utils.js ../../../../13-webgl/lib/simple-rotator.js ../../../../13-webgl/hws.edu-examples/teapot-model-IFS.js
-jsdoc -d doc-texture-cuon LightingWithTexture-cuon.js ../../../../WebGL/teal_book/cuon-matrix.js ../../../../WebGL/teal_book/cuon-utils.js ../../../../WebGL/lib/simple-rotator.js ../../../../WebGL/hws.edu-examples/teapot-model-IFS.js
+jsdoc -d doc-texture-cuon LightingWithTexture-cuon.js ../../../../13-webgl/lib/teal_book/cuon-matrix.js ../../../../13-webgl/lib/teal_book/cuon-utils.js ../../../../13-webgl/lib/simple-rotator.js ../../../../13-webgl/hws.edu-examples/teapot-model-IFS.js
 
