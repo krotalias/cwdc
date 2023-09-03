a19eeb1fef63df1c168d74ecafb8158d39c65af6
krotalias <32539739+krotalias@users.noreply.github.com>
2023-09-02 22:02:50 -0300

    Do not execute php

--- 13-webgl/sierpinski3/sierpinski3.js
+++ 13-webgl/sierpinski3/sierpinski3.js
@@ -130,7 +130,7 @@ var fractalScene = (loadedScene, maxLevel = 0, colorLevel = 0) => {
   var readFileNames = new Promise((resolve, reject) => {
     $.ajax({
       type: "GET",
-      url: "/cwdc/6-php/readFiles.php",
+      url: "/cwdc/6-php/readFiles_.php",
       data: {
         dir: "/cwdc/13-webgl/sierpinski3/models",
       },
