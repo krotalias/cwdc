c9ee6354cbc2536d96e686cf0df0418d6e521cb4
krotalias <32539739+krotalias@users.noreply.github.com>
2023-09-04 18:33:35 -0300

    Added RotatingSquare

---
+++ 13-webgl/homework/hw3/doc.sh
@@ -0,0 +1,14 @@
+jsdoc -d ../../doc-hierarchy Hierarchy.js
+    ../../lib/teal_book/cuon-matrix.js \
+    ../../lib/teal_book/cuon-utils.js \
+    ../../lib/simple-rotator.js
+jsdoc -d ../../doc-rot RotatingSquare.js
+    ../../lib/teal_book/cuon-matrix.js \
+    ../../lib/teal_book/cuon-utils.js
+jsdoc -d ../../doc-test test.js
+    ../../lib/teal_book/cuon-matrix.js \
+    ../../lib/teal_book/cuon-utils.js
+jsdoc -d ../../doc-test2 test2.js
+    ../../lib/teal_book/cuon-matrix.js \
+    ../../lib/teal_book/cuon-utils.js
+
