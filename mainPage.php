169aa67f3697f88db74ab0128b8a189addf6c215
Paulo R Cavalcanti <roma@dcc.ufrj.br>
2023-12-29 08:11:45 -0300

    Added bootstrap dropdown menu.

--- mainPage.php
+++ mainPage.php
@@ -6,17 +6,24 @@ include "mainPage/header2.php";
 <a href="midway.html" target="_blank">
 <div class="jumbotron" id="jumbotron">
     <h1 class="display-3">Comprehensive<br>Web Development<br>Course</h1>
-    <p>
-        <a class="lead" style="color:red" href="https://educador.brasilescola.uol.com.br/estrategias-ensino/como-usar-o-google-classroom.htm">Google</a>
+    <div class="ml-0">
+        <a
+            class="lead"
+            style="color:red"
+            href="https://educador.brasilescola.uol.com.br/estrategias-ensino/como-usar-o-google-classroom.htm">
+            Google
+        </a>
         <a style="color:#F5D58C" href="https://classroom.google.com/c/MTI4NjEzOTU0MjY4?cjc=w3axo3w">Classroom</a>
-    </p>
+    </div>
     <hr class="m-y-2">
-    <p style="color:red">Want to know more? <a style="color:#F5D58C" href="https://meet.google.com/syb-ngwg-xqs">Join us for our Weekly Meeting!</a></p>
+    <p style="color:red">Want to know more?
+        <a style="color:#F5D58C" href="https://meet.google.com/syb-ngwg-xqs">Join us for our Weekly Meeting!</a>
+    </p>
 </div>
 </a>
 
 <?php include "mainPage/navbar.php"; ?>
-    
+
 <script src="mainPage/main.js"></script>
 
 <?php include "mainPage/footer.php"; ?>
--- mainPage/navbar.php
+++ mainPage/navbar.php
@@ -1,3 +1,10 @@
+<?php
+
+// only get in from localhost, claro-net or vivo
+set_include_path(".:/usr/share/pear:/usr/share/php':/Users/roma/html/cwdc/6-php/");
+require "blockIPs.php";
+?>
+
 <nav class="navbar fixed-top navbar-expand-lg navbar-custom">
     <a class="navbar-brand" href="https://www.lcg.ufrj.br">
         <img src="/cwdc/mainPage/logo.png" style="height:48px;" alt="logo">
@@ -31,7 +38,36 @@
                 <a class="nav-link" href="/cwdc/4-jquery/index.php">jquery</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/cwdc/5-bootstrap/index.php">bootstrap</a>
+                <div class="dropdown mt-2 p-0" id="add_dropdown">
+                    <button
+                        type="button"
+                        class="btn border-0 dropdown-toggle"
+                        role="button"
+                        data-toggle="dropdown"
+                        style="padding: 0; color: lightgray"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                    >
+                        bootstrap
+                    </button>
+                    <div
+                        class="dropdown-menu lcg-menu"
+                        aria-labelledby="add_dropdown"
+                    >
+                        <a
+                            class="dropdown-item"
+                            href="/cwdc/5-bootstrap4/index.php"
+                        >
+                            Bootstrap 4
+                        </a>
+                        <a
+                            class="dropdown-item"
+                            href="/cwdc/5-bootstrap/index.php"
+                        >
+                            Bootstrap 5
+                        </a>
+                    </div>
+                </div>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/cwdc/6-php/index.php">php</a>
@@ -39,12 +75,14 @@
             <li class="nav-item">
                 <a class="nav-link" href="/cwdc/7-mysql/index.php">mysql</a>
             </li>
+            <!--
             <li class="nav-item">
                 <a class="nav-link" href="/cwdc/8-apis/index.php">apis</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/cwdc/9-mobileapps/index.php">mobileapps</a>
             </li>
+            -->
             <li class="nav-item">
                 <a class="nav-link" href="/cwdc/10-html5css3/index.php">html5css3</a>
             </li>
