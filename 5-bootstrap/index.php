c73c976ea81bc21a706dd38becf71dc0812dcaf8
Paulo R Cavalcanti <roma@dcc.ufrj.br>
2024-01-13 05:28:53 -0300

    Bootstrap 5

---
+++ 5-bootstrap/index.php
@@ -0,0 +1,81 @@
+<?php
+$title = "5 - bootstrap";
+include "../mainPage/header.php";
+include "../mainPage/navbar.php";
+?>
+
+<div class="lesson">
+    <h3> <a href="https://getbootstrap.com">Bootstrap 5</a> </h3>
+
+    <ol type="1" start="1">
+        <li>
+            <a href="5.2.html">Your First Bootstrap Site</a>
+            <a href="showCode.php?f=5.2"> (html)</a>
+        </li>
+        <li>
+            <a href="5.3.html">The Grid System</a>
+            <a href="showCode.php?f=5.3"> (html)</a>
+        </li>
+        <li>
+            <a href="5.4.html">Introducing Navbars</a>
+            <a href="showCode.php?f=5.4"> (html)</a>
+        </li>
+        <li>
+            <a href="../10-html5css3/clock/bootstrap/index.html">Clock</a>
+            <a href="showCode.php?f=../10-html5css3/clock/bootstrap/index"> (html)</a>
+            <a href="showCode.php?f=../10-html5css3/clock/bootstrap/navbar">
+            (navbar)
+        </a>
+        <li>
+            <a href="5.5.html">Forms and Tables</a>
+            <a href="showCode.php?f=5.5"> (html)</a>
+        </li>
+        <li>
+            <a href="5.6.html">Bootstrap Components</a>
+            <a href="showCode.php?f=5.6"> (html)</a>
+        </li>
+        <li>
+            <a href="5.7.html">Modals, Popovers & Tooltips</a>
+            <a href="showCode.php?f=5.7"> (html)</a>
+        </li>
+        <li>
+            <a href="5.8.html">ScrollSpy</a>
+            <a href="showCode.php?f=5.8"> (html)</a>
+        </li>
+        <li>
+            <a href="5.9.html">App Landing Page</a>
+            <a href="showCode.php?f=5.9"> (html)</a>
+        </li>
+        <li>
+            <a href="gestures.html">Gestures</a>
+            <a href="gestures.js"> (source)</a>
+            <a href="showCode.php?f=gestures"> (html)</a>
+        </li>
+    </ol>
+</div>
+<hr>
+
+<footer class="footer">
+    <div class="container-fluid" style="text-align: center;">
+        <a href="https://www.militaryfactory.com/aircraft/detail.asp?aircraft_id=262">
+            <figure>
+                <img
+                    title="Mitsubishi A6M Zero"
+                    alt="1940"
+                    src="Zeke.jpg"
+                    class="pimg"
+                >
+                <figcaption>
+                    Mitsubishi A6M Zero (1940 - 570 kmh)
+                </figcaption>
+            </figure>
+        </a>
+    </div>
+</footer>
+
+<script src="../mainPage/main.js"></script>
+
+<?php include "../mainPage/footer.php"; ?>
+
+</body>
+</html>
---
+++ 5-bootstrap/showcodePHP.php
@@ -0,0 +1,1 @@
+../6-php/showcode.php
