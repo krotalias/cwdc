fe4373a42c8976bb68be60bfd00ba870ca8ce72b
krotalias <32539739+krotalias@users.noreply.github.com>
2024-01-18 11:51:06 -0300

    generated doc-object

--- 13-webgl/homework/hw4/doc.sh
+++ 13-webgl/homework/hw4/doc.sh
@@ -1,2 +1,3 @@
 jsdoc -d doc-hier3 HierarchyWithTree3.js CS336Object.js ../../lib/teal_book/cuon-matrix.js ../../lib/teal_book/cuon-utils.js ../../lib/simple-rotator.js
 jsdoc -d doc-hier4 HierarchyWithTree4.js CS336Object.js ../../lib/teal_book/cuon-matrix.js ../../lib/teal_book/cuon-utils.js ../../lib/simple-rotator.js
+jsdoc -d doc-object CS336Object.js
