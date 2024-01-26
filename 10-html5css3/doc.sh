0bca4c8393f93d5563d7a1a544385e00d9edde5d
krotalias <32539739+krotalias@users.noreply.github.com>
2024-01-26 09:22:29 -0300

    Missing img directory.

--- 10-html5css3/doc.sh
+++ 10-html5css3/doc.sh
@@ -5,6 +5,6 @@ jsdoc -d docrectangles rectangles/rectangles.js
 jsdoc -d docsquares squares/squares.js
 jsdoc -d docDOM DOM.js
 jsdoc -c local.json -d docAsteroid asteroid/asteroid.js
-jsdoc -d docUtil circRec/util2d.js circRec/node_modules/gl-matrix/esm
+jsdoc -d docUtil circRec/util2d.js lib/gl-matrix/dist/esm
 jsdoc -d docCircRec circRec/circRec.js circRec/util2d.js lib/gl-matrix/dist/esm
 jsdoc -d docCircRecNoSource circRec/circRecNosource.js circRec/util2d.js lib/gl-matrix/dist/esm
