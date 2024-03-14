51ef761fcb61fa4c30abc0f06b4635860e0978c4
krotalias <32539739+krotalias@users.noreply.github.com>
2024-03-14 20:52:54 -0300

    Better html layout.

--- 13-webgl/homework/final/blobby.js
+++ 13-webgl/homework/final/blobby.js
@@ -1231,8 +1231,8 @@ function mainEntrance() {
    * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/resize_event
    */
   function handleWindowResize() {
-    let h = window.innerHeight;
-    let w = window.innerWidth;
+    let h = window.innerHeight - 16;
+    let w = window.innerWidth - 16;
     if (h > w) {
       h = w / aspect; // aspect < 1
     } else {
