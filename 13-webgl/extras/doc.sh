d7624e0787392378d10832c4ed85c6c65c08e6c8
krotalias <32539739+krotalias@users.noreply.github.com>
2024-05-22 09:14:31 -0300

    Changed Teapot definition on doc.

--- 13-webgl/extras/doc.sh
+++ 13-webgl/extras/doc.sh
@@ -1,3 +1,3 @@
 jsdoc -d doc-reflection reflectionMapSphere.js ../lib/simple-rotator.js ../lib/basic-objects-IFS.js ../lib/gl-matrix/dist/esm ../lib/polyhedron.js
-jsdoc -d doc-light LightingWithTexture.js      ../lib/simple-rotator.js ../lib/basic-objects-IFS.js ../lib/gl-matrix/dist/esm ../lib/polyhedron.js ../lib/teal_book/cuon-utils.js ../lib/TeapotGeometry.js
+jsdoc -d doc-light LightingWithTexture.js      ../lib/simple-rotator.js ../lib/basic-objects-IFS.js ../lib/gl-matrix/dist/esm ../lib/polyhedron.js ../lib/teal_book/cuon-utils.js ../examples/lighting/content/TeapotGeometry.js
 jsdoc -d doc-skybox skybox-and-env-map.js      ../lib/simple-rotator.js ../lib/basic-objects-IFS.js ../lib/gl-matrix/dist/esm
