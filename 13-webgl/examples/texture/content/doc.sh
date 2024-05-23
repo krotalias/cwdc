d49ddc94747c3d64e886f0c19d0ed5346726e909
Paulo R Cavalcanti <roma@dcc.ufrj.br>
2024-05-23 04:03:34 -0300

    TeapotGeometry-func.js

--- 13-webgl/examples/texture/content/doc.sh
+++ 13-webgl/examples/texture/content/doc.sh
@@ -1,3 +1,3 @@
-jsdoc -d doc-texture LightingWithTexture.js ../../../../13-webgl/lib/gl-matrix/dist/esm ../../../../13-webgl/lib/teal_book/cuon-utils.js ../../../../13-webgl/lib/simple-rotator.js ../../../../13-webgl/hws.edu-examples/teapot-model-IFS.js ../../../../13-webgl/lib/TeapotGeometry.js
+jsdoc -d doc-texture LightingWithTexture.js ../../../../13-webgl/lib/gl-matrix/dist/esm ../../../../13-webgl/lib/teal_book/cuon-utils.js ../../../../13-webgl/lib/simple-rotator.js ../../../../13-webgl/hws.edu-examples/teapot-model-IFS.js ../../../../13-webgl/lib/TeapotGeometry-func.js
 jsdoc -d doc-texture-cuon LightingWithTexture-cuon.js ../../../../13-webgl/lib/teal_book/cuon-matrix.js ../../../../13-webgl/lib/teal_book/cuon-utils.js ../../../../13-webgl/lib/simple-rotator.js ../../../../13-webgl/hws.edu-examples/teapot-model-IFS.js
 
