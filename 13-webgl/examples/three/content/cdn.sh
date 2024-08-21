5cdbc02d527618ab1d87b50e44bf372c713b5fa5
krotalias <32539739+krotalias@users.noreply.github.com>
2024-08-21 14:53:29 -0300

    Replacing unpkg also.

--- 13-webgl/examples/three/content/cdn.sh
+++ 13-webgl/examples/three/content/cdn.sh
@@ -1,3 +1,8 @@
 sed -i '' 's#/cwdc/13-webgl/lib/three.r163/examples/jsm/libs/draco#https://unpkg.com/three@latest/examples/jsm/libs/draco#g' stl.js
 sed -i '' 's#/cwdc/13\-webgl/lib/three.r163/examples/jsm#three/addons#g' stl.js
 sed -i '' 's#/cwdc/13\-webgl/lib/three.r163/build/three.module.js#three#g' stl.js
+
+sed -i '' 's#https://unpkg.com/three@latest/build/three.module.js#three#g' stl.js
+sed -i '' 's#https://unpkg.com/three@latest/examples/jsm#three/addons#g' stl.js
+sed -i '' 's#?module##g' stl.js
+sed -i '' 's#three/addons/libs/draco/gltf/#https://unpkg.com/three@latest/examples/jsm/libs/draco/gltf/#g' stl.js
