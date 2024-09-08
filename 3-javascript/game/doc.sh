4c4567d6ca36f5e878dde090ccc73be6d0d3bb4b
krotalias <32539739+krotalias@users.noreply.github.com>
2024-09-08 10:40:26 -0300

    Missing doc.

---
+++ 3-javascript/game/doc.sh
@@ -0,0 +1,6 @@
+jsdoc -d doc-game code/chapter/16_game.js code/levels.js
+#jsdoc -d doc-canvas code/chapter/17_canvas.js
+jsdoc -c jsdoc.conf
+jsdoc -d doc-level code/levels.js
+jsdoc -d doc-jack jack.js
+jsdoc -d doc-ball ball.js
