dd0ded353f2716401a4de3ab01014f51ea652d13
krotalias <32539739+krotalias@users.noreply.github.com>
2024-10-31 11:20:12 -0300

    Using namespace in MeshEdgesGeometry.js

--- 13-webgl/examples/three/content/MeshEdgesGeometry.js
+++ 13-webgl/examples/three/content/MeshEdgesGeometry.js
@@ -5,15 +5,15 @@
  *
  * Summary.
  *
- * <p>MeshEdgesGeometry is a simple class to extract edges from a Three.js {@link external:THREE.Object3D object}
+ * <p>MeshEdgesGeometry is a simple class to extract edges from a Three.js {@link THREE.Object3D object}
  * - a mesh or a group with children.</p>
  *
  * The hierarchy and substructures of the input object are flattened out.
  * Their positions, scales and rotations are baked into world coordinates.
  *
  * MeshEdgesGeometry is like {@link https://threejs.org/docs/#api/en/geometries/EdgesGeometry THREE.EdgesGeometry},
- * except that it does not work on {@link external:THREE.BufferGeometry},
- * but on {@link external:THREE.Object3D}.
+ * except that it does not work on {@link THREE.BufferGeometry},
+ * but on {@link THREE.Object3D}.
  *
  * @author {@link https://github.com/boytchev Pavel Boytchev}
  * @see <a href="/cwdc/13-webgl/examples/three/content/MeshEdgesGeometry.js">source</a>
@@ -22,13 +22,18 @@
  *      <img src="../LittlestTokyo.jpg" HEIGHT="320">
  */
 
-import { BufferGeometry, EdgesGeometry } from "three";
-import { mergeAttributes } from "three/addons/utils/BufferGeometryUtils.js";
+// import { BufferGeometry, EdgesGeometry } from "three";
+// import { mergeAttributes } from "three/addons/utils/BufferGeometryUtils.js?module";
+import {
+  BufferGeometry,
+  EdgesGeometry,
+} from "https://unpkg.com/three@latest/build/three.module.js?module";
+import { mergeAttributes } from "https://unpkg.com/three@latest/examples/jsm/utils/BufferGeometryUtils.js?module";
 
 /**
  * This is the base class for most objects in three.js and provides
  * a set of properties and methods for manipulating objects in 3D space.
- * @memberof external:THREE
+ * @memberof THREE
  * @class Object3D
  * @see {@link https://threejs.org/docs/#api/en/core/Object3D Object3D}
  */
@@ -40,7 +45,7 @@ import { mergeAttributes } from "three/addons/utils/BufferGeometryUtils.js";
  * See that page for details and a usage example.
  * When working with vector-like data, the .fromBufferAttribute( attribute, index )
  * helper methods on Vector2, Vector3, Vector4, and Color classes may be helpful.
- * @memberof external:THREE
+ * @memberof THREE
  * @class BufferAttribute
  * @see {@link https://threejs.org/docs/#api/en/core/BufferAttribute BufferAttribute}
  */
@@ -48,12 +53,12 @@ import { mergeAttributes } from "three/addons/utils/BufferGeometryUtils.js";
 /**
  * Class for extracting edges from gltf files.
  * @class
- * @extends external:THREE.BufferGeometry
+ * @extends THREE.BufferGeometry
  */
 class MeshEdgesGeometry extends BufferGeometry {
   /**
    * @constructor
-   * @param {external:THREE.Scene} object Any geometry object.
+   * @param {THREE.Scene} object Any geometry object.
    * @param {Number} thresholdAngle An edge is only rendered if the angle (in degrees) between<br>
    * the face normals of the adjoining faces exceeds this value. <br> default = 1 degree.
    */
@@ -66,10 +71,10 @@ class MeshEdgesGeometry extends BufferGeometry {
 
   /**
    * Extract edges - each edge is an individual segment.
-   * @param {external:THREE.Scene} object Any geometry object.
+   * @param {THREE.Scene} object Any geometry object.
    * @param {Number} thresholdAngle An edge is only rendered if the angle (in degrees) between<br>
    * the face normals of the adjoining faces exceeds this value. <br> default = 1 degree.
-   * @return {external:THREE.BufferAttribute} position buffer.
+   * @return {THREE.BufferAttribute} position buffer.
    * @see {@link https://threejs.org/docs/#examples/en/utils/BufferGeometryUtils.mergeAttributes mergeAttributes}
    */
   extractEdges(object, thresholdAngle) {
