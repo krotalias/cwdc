b09457b3737101722fe85eeb553f213826942896
krotalias <32539739+krotalias@users.noreply.github.com>
2025-01-01 07:53:40 -0300

    Fixed cookies.mjs path.

--- 10-html5css3/doc.sh
+++ 10-html5css3/doc.sh
@@ -1,8 +1,8 @@
 jsdoc -d docjs clock/clock.js clock/suncalc.js clock/date.js
 jsdoc -d doc-clock clock/clock-then.js clock/suncalc.js clock/date.js
-jsdoc -d docrectangles rectangles/rectangles.js
+jsdoc -c ../3-javascript/jsdoc.conf -d docrectangles rectangles/rectangles.js ../3-javascript/cookies/cookies.mjs
 #jsdoc -c jsdoc.json -d docsquares squares/squares.js
-jsdoc -d docsquares squares/squares.js
+jsdoc -c ../3-javascript/jsdoc.conf -d docsquares squares/squares.js ../3-javascript/cookies/cookies.mjs
 jsdoc -d docDOM DOM.js
 jsdoc -c local.json -d docAsteroid asteroid/asteroid.js
 jsdoc -d docUtil circRec/util2d.js lib/gl-matrix/dist/esm
