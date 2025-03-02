c87dbe007718be8f9d34fbd2245bb2bb75e648f4
krotalias <32539739+krotalias@users.noreply.github.com>
2025-03-02 11:43:34 -0300

    Added doc for misc_controls_arcball.js

--- 13-webgl/examples/three/content/doc.sh
+++ 13-webgl/examples/three/content/doc.sh
@@ -2,3 +2,4 @@ jsdoc -d doc-rot-three RotatingSquare.js
 jsdoc -d doc-euler-three EulerThreejs.js
 jsdoc -d doc-stl stl.js MeshEdgesGeometry.js
 jsdoc -d doc-example example.js
+jsdoc -d doc-cerberus misc_controls_arcball.js
