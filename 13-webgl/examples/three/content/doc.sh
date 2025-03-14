77eb328e08e7f4591d388d4ebc165ddc2181f6b4
krotalias <32539739+krotalias@users.noreply.github.com>
2025-03-14 10:41:36 -0300

    Exporting to GLTF.

--- 13-webgl/examples/three/content/doc.sh
+++ 13-webgl/examples/three/content/doc.sh
@@ -1,5 +1,5 @@
 jsdoc -d doc-rot-three RotatingSquare.js
-jsdoc -d doc-euler-three EulerThreejs.js
+jsdoc -d doc-euler-three EulerThreejs.js ../../../threejs-examples/ExportToGLTF.1.js
 jsdoc -d doc-stl stl.js MeshEdgesGeometry.js
 jsdoc -d doc-example example.js
 jsdoc -d doc-cerberus misc_controls_arcball.js
