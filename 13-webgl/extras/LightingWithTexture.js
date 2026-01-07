92cc8196d1197df3de136cb2be368a2ffdd42787
krotalias <32539739+krotalias@users.noreply.github.com>
2026-01-07 05:21:50 -0300

    Copyright.

--- 13-webgl/extras/LightingWithTexture.js
+++ 13-webgl/extras/LightingWithTexture.js
@@ -6,7 +6,7 @@
  * {@link https://web.engr.oregonstate.edu/~mjb/cs550/PDFs/TextureMapping.4pp.pdf texture mapping}
  * written in Vanilla Javascript and WebGL.</p>
  *
- * <p><a href="../images/Around_The_World_In_212_Historical_Figures.mp4">Around the World in 350 Historical Figures.</a>
+ * <p><a href="../images/Around_The_World_In_212_Historical_Figures.mp4">Around the World in 351 Historical Figures.</a>
  *
  * <p><b>For educational purposes only.</b></p>
  * <p>This is a <b><a href="../images/mapViewer.mp4">demo</a></b> for teaching {@link https://en.wikipedia.org/wiki/Computer_graphics CG},
@@ -167,7 +167,7 @@
  * or <a href="../doc/TeseKevinWeiler.pdf">radial-edge</a> data structures required in
  * {@link https://www.sciencedirect.com/science/article/abs/pii/S0010448596000668?via%3Dihub solid modeling}.
  *
- * <p><b>The application</b>: Around The World in <a href="../images/Brazil.mp4">350 historical figures</a>.</p>
+ * <p><b>The application</b>: Around The World in <a href="../images/Brazil.mp4">351 historical figures</a>.</p>
  * <p>When I was a child and forced to study history, I was never able to visualize the actual location of an event.
  * For instance, where were the locations of Thrace, Anatolia, Troy, the Parthian Empire, the Inca Empire, and Rapa Nui?</p>
  *
@@ -291,8 +291,8 @@
  * </ol>
  *
  * @author {@link https://krotalias.github.io Paulo Roma}
- * @license Licensed under the {@link https://www.opensource.org/licenses/mit-license.php MIT license}.
- * @copyright © 2024 Paulo R Cavalcanti.
+ * @license Licensed under the {@link https://www.gnu.org/licenses/lgpl-3.0.en.html LGPLv3}.
+ * @copyright © 2024-2026 Paulo R Cavalcanti.
  * @since 30/01/2016
  * @see <a href="/cwdc/13-webgl/extras/LightingWithTexture.html">link</a> - Texture coordinates sampled at each pixel in the {@link https://raw.githubusercontent.com/krotalias/cwdc/main/13-webgl/extras/LightingWithTexture.html fragment shader}
  * @see <a href="/cwdc/13-webgl/extras/LightingWithTexture2.html">link2</a> - Texture coordinates sampled at each vertex in the {@link https://raw.githubusercontent.com/krotalias/cwdc/main/13-webgl/extras/LightingWithTexture2.html vertex shader}
@@ -1025,7 +1025,7 @@ let colorShader;
  * Used only when rotating the model around the coordinate axes
  * {@link frame (intrinsic or extrinsic rotations)}
  * or along a meridian.
- * <p>A rotation along a parellel corresponds to an intrinsic
+ * <p>A rotation along a parallel corresponds to an intrinsic
  * rotation around the y-axis.</p>
  * @type {mat4}
  * @see {@link modelM}
