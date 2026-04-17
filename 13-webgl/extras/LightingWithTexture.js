8a2d98c7c4b77b926cc7b61aaaab9669aa3f819b
Paulo R Cavalcanti <roma@dcc.ufrj.br>
2026-04-17 16:15:26 -0300

    Added Music.

--- 13-webgl/extras/LightingWithTexture.js
+++ 13-webgl/extras/LightingWithTexture.js
@@ -6,7 +6,7 @@
  * {@link https://web.engr.oregonstate.edu/~mjb/cs550/PDFs/TextureMapping.4pp.pdf texture mapping}
  * written in {@link http://vanilla-js.com/ Vanilla Javascript} and {@link https://get.webgl.org/ WebGL}.</p>
  *
- * <p><a href="../images/Around_The_World_In_212_Historical_Figures.mp4">Around the World in 447 Historical Figures.</a>
+ * <p><a href="../images/Around_The_World_In_212_Historical_Figures.mp4">Around the World in 450 Historical Figures.</a>
  *
  * <p><b>For educational purposes only.</b></p>
  * <p>This is a <b><a href="../images/mapViewer.mp4">demo</a></b> for teaching {@link https://en.wikipedia.org/wiki/Computer_graphics CG},
@@ -187,7 +187,7 @@
  * or <a href="../doc/TeseKevinWeiler.pdf">radial-edge</a> data structures required in
  * {@link https://www.sciencedirect.com/science/article/abs/pii/S0010448596000668?via%3Dihub solid modeling}.
  *
- * <p><b>The application</b>: Around The World in <a href="../images/Brazil.mp4"> 447 historical figures</a>.</p>
+ * <p><b>The application</b>: Around The World in <a href="../images/Brazil.mp4"> 450 historical figures</a>.</p>
  * <p>When I was a child and forced to study history, I was never able to visualize the actual location of an event.
  * For instance, where were the locations of Thrace, Anatolia, Troy, the Parthian Empire, the Inca Empire, and Rapa Nui?</p>
  *
@@ -253,6 +253,7 @@
  * <li>🔬 means <i><a href="../images/science.mp4">Science</a></i>,</li>
  * <li>⛵ means <i><a href="../images/aod.mp4">Age of Discovery</a></i>,</li>
  * <li>⚓ means <i><a href="../images/Columbus.mp4">Columbus's voyages</a></i>,</li>
+ * <li>🎶 means <i><a href="../images/Music.mp4">Music</a></i>,</li>
  * <li>BC means <i><a href="https://www.instagram.com/reel/DRHRmcOD_KR/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">Before Christ</a></i> and</li>
  * <li>AD means <i><a href="https://en.wikipedia.org/wiki/Anno_Domini">Anno Domini</a></i>.</li>
  * </ul>
@@ -1749,6 +1750,9 @@ function setCountryDescription(country) {
       break;
     case "🔬":
       c = "STEM Members";
+      break;
+    case "🎶":
+      c = "Music";
       break;
     case "":
       c = "the world";
