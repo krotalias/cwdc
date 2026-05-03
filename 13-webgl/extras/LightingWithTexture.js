30e2578fbcb620cb0538ef34ecb503d8d12a361d
krotalias <32539739+krotalias@users.noreply.github.com>
2026-05-03 10:42:19 -0300

    Pink Floyd, Queen.

--- 13-webgl/extras/LightingWithTexture.js
+++ 13-webgl/extras/LightingWithTexture.js
@@ -6,7 +6,7 @@
  * {@link https://web.engr.oregonstate.edu/~mjb/cs550/PDFs/TextureMapping.4pp.pdf texture mapping}
  * written in {@link http://vanilla-js.com/ Vanilla Javascript} and {@link https://get.webgl.org/ WebGL}.</p>
  *
- * <p><a href="../images/Around_The_World_In_212_Historical_Figures.mp4">Around the World in 458 Historical Figures.</a>
+ * <p><a href="../images/Around_The_World_In_212_Historical_Figures.mp4">Around the World in 460 Historical Figures.</a>
  *
  * <p><b>For educational purposes only.</b></p>
  * <p>This is a <b><a href="../images/mapViewer.mp4">demo</a></b> for teaching {@link https://en.wikipedia.org/wiki/Computer_graphics CG},
@@ -187,7 +187,7 @@
  * or <a href="../doc/TeseKevinWeiler.pdf">radial-edge</a> data structures required in
  * {@link https://www.sciencedirect.com/science/article/abs/pii/S0010448596000668?via%3Dihub solid modeling}.
  *
- * <p><b>The application</b>: Around The World in <a href="../images/Brazil.mp4"> 458 historical figures</a>.</p>
+ * <p><b>The application</b>: Around The World in <a href="../images/Brazil.mp4"> 460 historical figures</a>.</p>
  * <p>When I was a child and forced to study history, I was never able to visualize the actual location of an event.
  * For instance, where were the locations of Thrace, Anatolia, Troy, the Parthian Empire, the Inca Empire, and Rapa Nui?</p>
  *
@@ -1942,8 +1942,8 @@ function labelForLocation(location, unit) {
     <br>Rio &rarr; ${clocation}:
           ${fmtDistance(drio, unit)},
           AZ: ${fmtdeg.format(brio)}
-    <br>Rio ↛ ${clocation}
-          along loxodrome: ${fmtDistance(ldrio, unit)}
+    <br>Rio ↛ ${clocation} along loxodrome:
+          ${fmtDistance(ldrio, unit)}
     <br>${plocation} &rarr; ${clocation}:
           ${fmtDistance(distancep, unit)},
           AZ: ${fmtdeg.format(bp)}
