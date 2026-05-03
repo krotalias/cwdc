fba2262d2e6460d77f402a3039fcb88922affd02
krotalias <32539739+krotalias@users.noreply.github.com>
2026-05-03 18:13:49 -0300

    Marathon and Sandia.

--- 13-webgl/extras/LightingWithTexture.js
+++ 13-webgl/extras/LightingWithTexture.js
@@ -6,7 +6,7 @@
  * {@link https://web.engr.oregonstate.edu/~mjb/cs550/PDFs/TextureMapping.4pp.pdf texture mapping}
  * written in {@link http://vanilla-js.com/ Vanilla Javascript} and {@link https://get.webgl.org/ WebGL}.</p>
  *
- * <p><a href="../images/Around_The_World_In_212_Historical_Figures.mp4">Around the World in 460 Historical Figures.</a>
+ * <p><a href="../images/Around_The_World_In_212_Historical_Figures.mp4">Around the World in 461 Historical Figures.</a>
  *
  * <p><b>For educational purposes only.</b></p>
  * <p>This is a <b><a href="../images/mapViewer.mp4">demo</a></b> for teaching {@link https://en.wikipedia.org/wiki/Computer_graphics CG},
@@ -187,7 +187,7 @@
  * or <a href="../doc/TeseKevinWeiler.pdf">radial-edge</a> data structures required in
  * {@link https://www.sciencedirect.com/science/article/abs/pii/S0010448596000668?via%3Dihub solid modeling}.
  *
- * <p><b>The application</b>: Around The World in <a href="../images/Brazil.mp4"> 460 historical figures</a>.</p>
+ * <p><b>The application</b>: Around The World in <a href="../images/Brazil.mp4"> 461 historical figures</a>.</p>
  * <p>When I was a child and forced to study history, I was never able to visualize the actual location of an event.
  * For instance, where were the locations of Thrace, Anatolia, Troy, the Parthian Empire, the Inca Empire, and Rapa Nui?</p>
  *
@@ -534,7 +534,7 @@ let globeRadius = sphereRadius;
  * Current device ppi (pixel per inch).
  * @type {Number}
  */
-let ppiIndex = 2;
+let ppiIndex = 1;
 
 /**
  * Whether the control key has been held down.
@@ -3373,11 +3373,11 @@ function drawLinesOnImage() {
     const remarkable = gpsCoordinates[currentLocation].remarkable;
     const colon = remarkable.at(-1).indexOf(":");
     if (bearingAngle !== null) {
+      const AZ = `Bearing Angle: ${fmtdeg.format(bearingAngle)}`;
       if (colon > -1) {
-        remarkable[remarkable.length - 1] =
-          `Bearing Angle: ${fmtdeg.format(bearingAngle)}`;
+        remarkable[remarkable.length - 1] = AZ;
       } else {
-        remarkable.push(`Bearing Angle: ${fmtdeg.format(bearingAngle)}`);
+        remarkable.push(AZ);
       }
     } else {
       if (colon > -1) {
@@ -6159,7 +6159,8 @@ function startForReal(image) {
   cities.current = getCitiesSelector();
 
   document.getElementById("doc").innerHTML +=
-    `(Earth radius = <span style="color:#FF0000">${fmtkm.format(earthRadius)}</span>)`;
+    `(Earth radius = <span style="color:#FF0000">
+      ${fmtkm.format(earthRadius)}</span>)`;
 
   // Phong highlight position: (0,0,1) = {-90,0} in GCS
   const coordinates = gcs2Screen({ longitude: -90, latitude: 0 }, false);
