d402552e8f0c222039073e2f3408766f90c3d565
Paulo R Cavalcanti <roma@dcc.ufrj.br>
2026-06-11 22:48:23 -0300

    Testing for screen.orientation.

--- 13-webgl/extras/LightingWithTexture.js
+++ 13-webgl/extras/LightingWithTexture.js
@@ -6,7 +6,7 @@
  * {@link https://web.engr.oregonstate.edu/~mjb/cs550/PDFs/TextureMapping.4pp.pdf texture mapping}
  * written in {@link http://vanilla-js.com/ Vanilla Javascript} and {@link https://get.webgl.org/ WebGL}.</p>
  *
- * <p><a href="../images/Around_The_World_In_212_Historical_Figures.mp4">Around the World in 463 Historical Figures.</a>
+ * <p><a href="../images/Around_The_World_In_212_Historical_Figures.mp4">Around the World in 464 Historical Figures.</a>
  *
  * <p><b>For educational purposes only.</b></p>
  * <p>This is a <b><a href="../images/mapViewer.mp4">demo</a></b> for teaching {@link https://en.wikipedia.org/wiki/Computer_graphics CG},
@@ -189,7 +189,7 @@
  * or <a href="../doc/TeseKevinWeiler.pdf">radial-edge</a> data structures required in
  * {@link https://www.sciencedirect.com/science/article/abs/pii/S0010448596000668?via%3Dihub solid modeling}.
  *
- * <p><b>The application</b>: Around The World in <a href="../images/Brazil.mp4"> 463 historical figures</a>.</p>
+ * <p><b>The application</b>: Around The World in <a href="../images/Brazil.mp4"> 464 historical figures</a>.</p>
  * <p>When I was a child and forced to study history, I was never able to visualize the actual location of an event.
  * For instance, where were the locations of Thrace, Anatolia, Troy, the Parthian Empire, the Inca Empire, and Rapa Nui?</p>
  *
@@ -6811,7 +6811,7 @@ function startForReal(image) {
    */
   window.addEventListener("resize", (event) => {
     displayVersions(ppiIndex);
-    // handleWindowResize;
+    // handleWindowResize();
   });
 
   /**
@@ -6823,10 +6823,13 @@ function startForReal(image) {
    * @event orientationChange
    * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/CSS_Object_Model/Managing_screen_orientation Managing screen orientation}
    */
-  screen.orientation.addEventListener("change", (event) => {
-    displayVersions(ppiIndex);
-    // handleWindowResize;
-  });
+  // Check if the modern API exists and has a type
+  if (screen.orientation && screen.orientation.type) {
+    screen.orientation.addEventListener("change", (event) => {
+      displayVersions(ppiIndex);
+      // handleWindowResize();
+    });
+  }
 
   gl = canvas.getContext("webgl2", { preserveDrawingBuffer: true });
   if (!gl) {
